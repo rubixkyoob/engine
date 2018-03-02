1c71493f18cff7b58d0a40ac2d18cdc0d5c3bee8
Peter Kalmar <rubixkyoob2@yahoo.com>
2018-03-02 14:04:06 -0500

    beginning Input

--- Input.js
+++ Input.js
@@ -5,4 +5,20 @@ class Input {
 	}
 }
 
-Input.getKeyDown
+Input.key_pressed = {};
+Input.key_down = {};
+Input.key_released = {};
+
+$(document).ready(function() {
+	
+	window.addEventListener("onkeydown", function(e) {
+		Input.key_pressed[]
+	}, false);
+});
+
+Input.GetKeyPressed = function(key) {
+	if(Input.key_pressed[key] == undefined) {
+		Input.key_pressed[key] = false;
+	}
+	return Input.key_pressed[key];
+}
